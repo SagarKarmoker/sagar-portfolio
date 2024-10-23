63a0d51e6218db0fe33798430ec8c93e490b9ee6
Sagar Karmoker <98447327+SagarKarmoker@users.noreply.github.com>
2024-10-23 17:51:59 +0000

    setup

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-center text-4xl flex justify-center items-center h-screen">
+      <h2>Sagar Karmoker Portfolio Website</h2>
+    </main>
+  );
+}
