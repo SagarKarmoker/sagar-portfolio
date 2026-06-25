c08498e96d76787c2df986af5f8c901fe42fde31
Sagar Karmoker <sagarkarmoker.official@gmail.com>
2026-06-25 18:11:00 +0600

    feat: refactor route handling for keystatic API; implement conditional handler initialization and remove unused page component

--- app/api/keystatic/[[...params]]/route.js
+++ app/api/keystatic/[[...params]]/route.js
@@ -1,4 +1,17 @@
-import { makeRouteHandler } from '@keystatic/next/route-handler'
-import config from '../../../../keystatic.config'
+const isEnabled = !!(
+  process.env.KEYSTATIC_GITHUB_CLIENT_ID &&
+  process.env.KEYSTATIC_GITHUB_CLIENT_SECRET &&
+  process.env.KEYSTATIC_SECRET
+);
 
-export const { GET, POST } = makeRouteHandler({ config })
+let handler;
+if (isEnabled) {
+  const { makeRouteHandler } = await import('@keystatic/next/route-handler');
+  const config = (await import('../../../../keystatic.config')).default;
+  handler = makeRouteHandler({ config });
+} else {
+  handler = {};
+}
+
+export const GET = handler.GET ?? (() => new Response('Not Found', { status: 404 }));
+export const POST = handler.POST ?? (() => new Response('Not Found', { status: 404 }));
